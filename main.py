d6eec1b309e0416da44c2741949554bb4521bcf0
Dariusz Okonski <darek200180@gmail.com>
2020-12-12 18:27:17 +0000

    add combobox components

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import StringVar
+from tkinter import StringVar, ttk
 
 root = tkinter.Tk()
 root.title('Metric Helper')
@@ -15,33 +15,34 @@ root.config(bg=bg_color)
 # define functions
 
 # define layout
-input_field = tkinter.Entry(root, widt=20, font=field_font)
-output_field = tkinter.Entry(root, width=20, font=field_font)
+input_field = tkinter.Entry(root, widt=20, font=field_font, borderwidth=3)
+output_field = tkinter.Entry(root, width=20, font=field_font, borderwidth=3)
 equal_label = tkinter.Label(root, text="=", font=field_font, bg=bg_color)
 
-input_field.grid(row=0, column=0)
-equal_label.grid(row=0, column=1)
-output_field.grid(row=0, column=2)
+input_field.grid(row=0, column=0, padx=10, pady=10)
+equal_label.grid(row=0, column=1, padx=10, pady=10)
+output_field.grid(row=0, column=2, padx=10, pady=10)
 
 input_field.insert(0, 'Enter your quantity')
 
-metric_list = ["femto", "pico", "nano", "micro", "milli", "centi", "deci", "base value", "deca", "hecto", "kilo", "mega", "giga", "tera", "peta"]
-input_choice = StringVar()
-input_choice.set("base value")
-output_choice = StringVar()
-output_choice.set("base value")
-
-input_dropdown = tkinter.OptionMenu(root, input_choice, *metric_list)
-output_dropdown = tkinter.OptionMenu(root, output_choice, *metric_list)
+# create combobox for metric values
 to_label = tkinter.Label(root, text=" to ", font=field_font, bg=bg_color)
+metric_list = ["femto", "pico", "nano", "micro", "milli", "centi", "deci", "base value", "deca", "hecto", "kilo", "mega", "giga", "tera", "peta"]
+input_combobox = ttk.Combobox(root, value=metric_list, font=field_font, justify='center')
+output_combobox = ttk.Combobox(root, value=metric_list, font=field_font, justify='center')
+input_combobox.grid(row=1, column=0, padx=10, pady=10)
+to_label.grid(row=1, column=1, padx=10, pady=10)
+output_combobox.grid(row=1, column=2, padx=10, pady=10)
 
-input_dropdown.grid(row=1, column=0)
-to_label.grid(row=1, column=1)
-output_dropdown.grid(row=1, column=2)
+input_combobox.set("base value")
+output_combobox.set("base value")
+
+
+
 
 # create conversion buttons
 convert_button = tkinter.Button(root, text='Convert', font=field_font, bg=button_color)
-convert_button.grid(row=2, column=0, columnspan=3)
+convert_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10, ipadx=50)
 
 
 
